f8da774b6ba59ec4b40c4bab0fb3d54e2179dbb0
jaeduck1507 <67354562+jaeduck1507@users.noreply.github.com>
2025-07-16 14:50:08 +0900

    Initial commit

---
+++ src/test/java/com/project/erp/CtrlsErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlsErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
