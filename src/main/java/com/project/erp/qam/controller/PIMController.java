f7db6d5a7942582ba3123c849adc469068098a4c
euijinyunbiz <euijinyunbiz@gmail.com>
2025-07-21 15:13:47 +0900

    .

--- src/main/java/com/project/erp/qam/controller/PIMController.java
+++ src/main/java/com/project/erp/qam/controller/PIMController.java
@@ -2,6 +2,30 @@
 
 package com.project.erp.qam.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.project.erp.qam.model.vo.Product;
+import com.project.erp.qam.service.ProductService;
+
+@Controller
 public class PIMController {
 
+	@Autowired
+	private ProductService productService;
+	
+	@ResponseBody
+	@GetMapping("/showProduct")
+	public List<Product> showProduct() {
+		return productService.showProduct();
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/com/project/erp/qam/model/vo/Product.java
+++ src/main/java/com/project/erp/qam/model/vo/Product.java
@@ -2,6 +2,8 @@ package com.project.erp.qam.model.vo;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,8 @@ import lombok.NoArgsConstructor;
 public class Product { //제품 테이블: 실제 생산된 개별 제품의 정보를 관리하는 테이블
 	
  private int productNo; // 제품 고유 번호 (PK, AUTO_INCREMENT)
+ 
+ @JsonFormat(pattern = "yyyy-MM-dd")
  private LocalDate productionDate; // 제품 생산일 (예: 2025-07-01)
  private int productCode; // 제품 코드 (FK, product_name.product_code 참조)
 
