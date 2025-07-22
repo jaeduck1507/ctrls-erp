6fab65104f4bfb05681ef5fd23be5c1cde4c0535
jaeduck1507 <67354562+jaeduck1507@users.noreply.github.com>
2025-07-22 16:00:46 +0900

    사원 일괄 등록

--- src/main/java/com/project/erp/hrm/controller/HrmPageController.java
+++ src/main/java/com/project/erp/hrm/controller/HrmPageController.java
@@ -2,18 +2,27 @@ package com.project.erp.hrm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.erp.hrm.service.DepartmentService;
+import com.project.erp.hrm.service.EmployeeInfoService;
 import com.project.erp.hrm.service.JobPositionService;
 
 @RequestMapping("/hrm")
 @Controller
 public class HrmPageController {
 
+	@Autowired
+	private EmployeeInfoService employeeInfoservice;
 	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@Autowired
+	private JobPositionService jobPositionService;
 	
 	@GetMapping("/empInfo")
 	public String empInfo() {
@@ -21,14 +30,21 @@ public class HrmPageController {
 		return "component/hrm/empInfo";
 	}
 	
-	@GetMapping("empAdd")
-	public String empAdd() {
+	@GetMapping("/empAdd")
+	public String empAdd(Model model) {
+		model.addAttribute("department", departmentService.showDept());
+		model.addAttribute("jobPosition", jobPositionService.showJob());
 		return "component/hrm/empAdd";
 	}
 	
 	@GetMapping("/quitShow")
 	public String quitter() {
 		return "component/hrm/quitShow";
+	}
+	
+	@GetMapping("/empEval")
+	public String empEval(Model model) {
+		return "component/hrm/empEval";
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/component/hrm/empEval.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>직원정보조회</title>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</head>
+<body>
+	<h1>인사 평가</h1>
+	
+	
+	
+	
+	
+	
+	<script>
+		
+		
+		
+		
+    </script>
+</body>
+</html>
