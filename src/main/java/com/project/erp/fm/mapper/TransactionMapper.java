bfd061289dc9445f9ed43abff184964e966ec60a
Jiyeon232 <wldus23299@gmail.com>
2025-08-08 14:06:40 +0900

    feat: fm 페이징 처리 시작

--- src/main/java/com/project/erp/fm/mapper/TransactionMapper.java
+++ src/main/java/com/project/erp/fm/mapper/TransactionMapper.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.project.erp.fm.model.dto.TransDTO;
+import com.project.erp.fm.model.dto.TransPagingDTO;
 import com.project.erp.fm.model.vo.Transaction;
 
 @Mapper
 public interface TransactionMapper {
 
 	List<TransDTO> showtrans(TransDTO trans);
+	List<TransDTO> showtransPage(TransPagingDTO transPaging);
+	int totalTrans(TransDTO trans);
 	void transRegister(List<Transaction> tList);
 }
--- src/main/java/com/project/erp/fm/service/TransactionService.java
+++ src/main/java/com/project/erp/fm/service/TransactionService.java
@@ -5,23 +5,43 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.erp.common.model.vo.Paging;
 import com.project.erp.fm.mapper.TransactionMapper;
 import com.project.erp.fm.model.dto.TransDTO;
+import com.project.erp.fm.model.dto.TransPagingDTO;
 import com.project.erp.fm.model.vo.Transaction;
 
 @Service
-public class TransactionService implements TransactionMapper {
+public class TransactionService {
 
 	@Autowired
-	private TransactionMapper mapper;
+	private TransactionMapper transactionMapper;
 
-	@Override
 	public List<TransDTO> showtrans(TransDTO trans) {
-		return mapper.showtrans(trans);
+		return transactionMapper.showtrans(trans);
 	}
 
-	@Override
-	public void transRegister(List<Transaction> tList) {
-		mapper.transRegister(tList);		
+	public List<TransDTO> showtransPage(TransPagingDTO transPaging) {
+		//paging.setOffset(paging.getLimit() * (paging.getPage() - 1));
+		return transactionMapper.showtransPage(transPaging);
 	}
+	
+	public int totalTrans(TransDTO trans) {
+		return transactionMapper.totalTrans(trans);
+	}
+	
+	public TransPagingDTO showTransPaging(TransDTO trans, Paging paging) {
+		TransPagingDTO transPagingDTO = new TransPagingDTO();
+		transPagingDTO.setTransDTO(trans);
+		transPagingDTO.setOffset(paging.getLimit() * (paging.getPage() - 1));
+		transPagingDTO.setTransList(showtransPage(transPagingDTO));
+		transPagingDTO.setPage(paging.getPage());
+		transPagingDTO.setTotal(totalTrans(trans));
+		return transPagingDTO;
+	}
+	
+	public void transRegister(List<Transaction> tList) {
+		transactionMapper.transRegister(tList);		
+	}
+	
 }
